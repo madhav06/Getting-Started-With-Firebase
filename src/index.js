3781ba3f279923c0bdca5e74dfd30542916d40e5
madhav06 <mnandan06@gmail.com>
2022-04-30 00:00:12 +0530

    firestore setup, fetching data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('hello from index.js')
+import { initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBz9YNK6fvd97fD-wmKAumTGtwFcPq_R80",
+    authDomain: "fir-9-endgame.firebaseapp.com",
+    projectId: "fir-9-endgame",
+    storageBucket: "fir-9-endgame.appspot.com",
+    messagingSenderId: "496453604155",
+    appId: "1:496453604155:web:7e9f7fc8df2ab531891d1e"
+  };
+
+// init firebase app
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'paintings')
+
+// get collection data
